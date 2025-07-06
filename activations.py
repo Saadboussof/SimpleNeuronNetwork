2cedbb18592103f340477cb94713320d98321465
saadboussof <drqsaad.boss2518@email.com>
2025-07-06 01:55:25 +0100

    simple neuronNetwork

---
+++ activations.py
@@ -0,0 +1,34 @@
+import numpy as np
+from layer import Layer
+from activation import Activation
+
+class ReLU(Activation):
+    def __init__(self):
+        def relu(x):
+            return np.maximum(0, x)
+
+        def relu_prime(x):
+            return (x > 0).astype(float)
+
+        super().__init__(relu, relu_prime)
+
+class Sigmoid(Activation):
+    def __init__(self):
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+
+        super().__init__(sigmoid, sigmoid_prime)
+
+class Softmax(Layer):
+    def forward(self, input):
+        tmp = np.exp(input)
+        self.output = tmp / np.sum(tmp)
+        return self.output
+    
+    def backward(self, output_gradient, learning_rate):
+        n = np.size(self.output)
+        return np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
